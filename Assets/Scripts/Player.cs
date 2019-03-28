ec5502ffd5a5cc6fbdd8a355e84e1db2c934d0ea
ArsenS <sylvain.arsenault@gmail.com>
2019-03-28 15:41:06 -0400

    PacMan movement done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
+    private Rigidbody2D rb2D;
     private CircleCollider2D circleCollider;
     private Animator animator;
     private Vector2 direction;
@@ -13,41 +13,58 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>();
         circleCollider = GetComponent<CircleCollider2D>();
         animator = GetComponent<Animator>();
-        speed = 1.0f;
+        speed = 2.0f;
         direction = new Vector2(0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetDirection();
+        UpdateDirection();
     }
 
     void FixedUpdate()
     {
-
+        if (CanMove(direction))
+        {
+            rb2D.MovePosition(rb2D.position + (direction * speed) * Time.fixedDeltaTime);
+        }
+        print(rb2D.IsSleeping());
     }
 
-    void GetDirection()
+    void UpdateDirection()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && CanMove(Vector2.up))
         {
+            direction = Vector2.up;
             animator.SetTrigger("movingUp");
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) && CanMove(Vector2.down))
         {
+            direction = Vector2.down;
             animator.SetTrigger("movingDown");
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && CanMove(Vector2.left))
         {
+            direction = Vector2.left;
             animator.SetTrigger("movingLeft");
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && CanMove(Vector2.right))
         {
+            direction = Vector2.right;
             animator.SetTrigger("movingRight");
         }
+    }
+
+    bool CanMove(Vector2 direction)
+    {
+        Vector2 pos = transform.position;
+        circleCollider.enabled = false; // disable to avoid colliding with itself
+        RaycastHit2D hit = Physics2D.BoxCast(pos, new Vector2(0.18f, 0.18f), 0f, direction, 0.05f);
+        circleCollider.enabled = true;
+        return (hit.collider == null);
     }
 }
