1405988ab43b4db77b0b9a7c02c7bc84528b7879
ArsenS <sylvain.arsenault@gmail.com>
2019-03-28 14:31:37 -0400

    PacMan animations set up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    private CircleCollider2D circleCollider;
+    private Animator animator;
+    private Vector2 direction;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+        animator = GetComponent<Animator>();
+        speed = 1.0f;
+        direction = new Vector2(0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetDirection();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void GetDirection()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetTrigger("movingUp");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetTrigger("movingDown");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetTrigger("movingLeft");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetTrigger("movingRight");
+        }
     }
 }
